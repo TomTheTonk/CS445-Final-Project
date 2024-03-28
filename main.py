313c9e917e0f81564add14ac20b22b0468dce967
Thomas M Donnelly <58053836+TomTheTonk@users.noreply.github.com>
2024-03-28 07:45:45 -0500

    Update

--- main.py
+++ main.py
@@ -55,10 +55,11 @@ def imshow(img):
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 40, 5) #was 30 output for early tests
+        self.conv1 = nn.Conv2d(3, 50, 5) #was 30 output for early tests
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(40, 16, 5)
-        self.fc1 = nn.Linear(1296, 120)
+        self.conv2 = nn.Conv2d(50, 32, 5)
+        self.conv3 = nn.Conv2d(32, 16, 5)
+        self.fc1 = nn.Linear(64, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
        
@@ -66,6 +67,7 @@ class Net(nn.Module):
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
         x = torch.flatten(x, 1) # flatten all dimensions except batch
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
